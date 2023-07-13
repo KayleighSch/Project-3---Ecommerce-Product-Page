07c27655133ba63ba69ea191274bb866e14168e5
Kayleigh Schmassmann <124618333+Schma2k1988@users.noreply.github.com>
2023-07-13 23:16:09 +0100

    event listeners added

--- script.js
+++ script.js
@@ -131,3 +131,14 @@ images.forEach((image) => {
       }
     });
   });
+ 
+menuBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+plusBtn.addEventListener("click", handlePlus);
+minusBtn.addEventListener("click", handleMinus);
+nextBtn.addEventListener("click", nextImage);
+prevBtn.addEventListener("click", prevImage);
+cartBtn.addEventListener("click", toggleCart);
+closeLightboxBtn.addEventListener("click", closeLightBox);
+mainThumbnail.addEventListener("click", openLightBox);
+addBtn.addEventListener("click", addItem);
